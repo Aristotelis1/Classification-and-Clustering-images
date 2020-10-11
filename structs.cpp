5723f26ec6e77e7f711e32a401f9a8a3b078a5c3
Aristotelis T <aristotelis.tsoutsanis@gmail.com>
2020-10-12 00:05:07 +0300

    v4

--- structs.cpp
+++ structs.cpp
@@ -9,49 +9,113 @@
 using namespace std;
 
 
+// vector<unsigned char>* hash_list::get_image()
+// {
+    
+// }
+
+// image::image(vector<unsigned char> i)
+// {
+//     img = &i;
+// }
+
+void Hash_list::display_list()
+{
+
+    for (auto &l : list_of_images)
+    {
+        //cout << "list of images size: " << list_of_images.size();
+        //cout << "->" <<  (unsigned int)l->at(0) << endl;
+
+        cout << "-->" ;
+        for(int i = 0; i < l->size(); i++)
+        {
+            cout << "-" << (unsigned int)l->at(i);
+        }
+    }
+    cout << endl;
+}
+
+Hash_list::Hash_list()
+{
+    for (auto &l : list_of_images)
+    {
+        cout << l->empty();
+    }
+}
+
+void Hash_list::add_image(vector<unsigned char> &i)
+{
+    //image im(i);
+    list_of_images.push_back(&i);
+}
+
+void Hash_list::clear()
+{
+    for (auto &l : list_of_images)
+    {
+        l->clear();
+        cout << "clear" << endl;
+        cout << l->empty();
+    }
+}
+
+void Hash_list::searchByKey()
+{
+    for (auto &l : list_of_images)
+    {
+        //cout << "list of images size: " << list_of_images.size();
+        //cout << "->" <<  (unsigned int)l->at(0) << endl;
+
+        cout << "-->" ;
+        for(int i = 0; i < l->size(); i++)
+        {
+            cout << "-" << (unsigned int)l->at(i);
+        }
+    }
+    cout << endl;
+}
+
 Hash::Hash(int b)
 {
     this->bucket = b; //size of the hash table
-    table = new list< vector<unsigned char> >[bucket];
+    // table = new array<list< vector<unsigned char>>,bucket>;
+    hash_table = new Hash_list[bucket];
 
+    for(int i = 0; i < bucket; i++)
+    {
+        //cout << i << endl;;
+        
+        hash_table[i].clear();
+    }
 }
 
 Hash::~Hash()
 {
-    delete[] table;
+    delete[] hash_table;
 }
 
-void Hash::insertItem(vector<unsigned char> key)
+void Hash::insertItem(vector<unsigned char> &image)
 {
     int index = 2; // we call the hash function
-    table[index].push_back(key);
+    hash_table[index].add_image(image);
+    //table[index].push_back(image);
 }
 
 void Hash::displayHash()
 {
-    vector<unsigned char>::iterator vec_it;
-    list< vector<unsigned char> >::iterator list_it;
-    for(int i =0; i < bucket; i++)
+    list<vector<unsigned char>*>::iterator it;
+    for(int i = 0; i < bucket; i++)
     {
-        cout << i;
-        // for (vector<unsigned char> x : table[i])
-        // {
-        //     cout << " --> " << x.data();
-        // }
-
-        for(list_it = table[i].begin(); list_it != table[i].end(); ++ list_it)
-        {
-            //printf("lalaa\n");
-            for(vec_it = list_it->begin(); vec_it != list_it->end(); ++vec_it)
-            {
-                //cout << vec_it;
-                
-                printf("------>%u", (unsigned)(*vec_it));
-            }
-        }
-        cout << endl;
-
+        cout << i ;
+        hash_table[i].display_list();
     }
+}
+
+void Hash::searchByKey(int index)
+{
+    // search by key
+    hash_table[index].searchByKey();
 }
 
 
--- structs.h
+++ structs.h
@@ -3,25 +3,51 @@
 
 #include <string>
 #include <list>
+#include <array>
 #include "image.h"
 
 using namespace std;
+
+// class image
+// {
+//     private:
+//         vector<unsigned char>* img;
+//     public:
+//         image(vector<unsigned char> i);
+
+// };
+
+class Hash_list
+{
+    private:
+        list<vector<unsigned char>*> list_of_images;
+    public:
+        Hash_list();
+        void add_image(vector<unsigned char> &i);
+        void display_list();
+        void clear();
+        void searchByKey();
+};
 
 
 
 class Hash {
     private:
         int bucket;
-        list< vector<unsigned char> > *table;
+        // array< list< vector<unsigned char>*>> *table;
+        Hash_list* hash_table;
+        
     public:
         Hash(int v); // Constructor
 
         ~Hash();
 
         // insert
-        void insertItem(vector<unsigned char> i);
+        void insertItem(vector<unsigned char> &i);
 
         void displayHash();
+
+        void searchByKey(int index);
 
 };
 
